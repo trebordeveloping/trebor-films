b1cd720340c928cba306f65b945a0f3d026f46dc
trebordeveloping <robert3cormack@gmail.com>
2024-02-27 23:13:09 +0100

    header design

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,36 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 
+import "./Header.css";
+
 export default function Header() {
+
+    const activeStyle = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+    }
     return (
         <header>
             <Link className="site-logo" to="/">TreborFilms</Link>
             <nav>
-                <NavLink to="films">Films</NavLink>
-                <NavLink to="about">About</NavLink>
-                <NavLink to="account">Account</NavLink>
+                <NavLink
+                    to="films"
+                    style={({isActive}) => isActive ? activeStyle : null}
+                >
+                    Films
+                </NavLink>
+                <NavLink
+                    to="about"
+                    style={({isActive}) => isActive ? activeStyle : null}
+                >
+                    About
+                </NavLink>
+                <NavLink
+                    to="account"
+                    style={({isActive}) => isActive ? activeStyle : null}
+                >
+                    Account
+                </NavLink>
             </nav>
         </header>
     )
