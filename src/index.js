a2eb140bb33e7b42950d3b5ad0dc696b4918b857
trebordeveloping <robert3cormack@gmail.com>
2024-02-27 23:22:20 +0100

    using app in index.js

--- src/index.js
+++ src/index.js
@@ -27,6 +27,6 @@ function App() {
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
