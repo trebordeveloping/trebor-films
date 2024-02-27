74f48ab4c9acdb6d5f5476591d7473f263425352
trebordeveloping <robert3cormack@gmail.com>
2024-02-27 22:39:49 +0100

    set up browser router and router provider

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from 'react-router-dom';
 import './index.css';
-import App from './App';
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<h1>hi</h1>} />
+))
+
+function App() {
+  return (
+    <RouterProvider router={router} />
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
