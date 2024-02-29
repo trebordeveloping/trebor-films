174b61842c09daeae93e2ff3783d602223bf09b0
trebordeveloping <robert3cormack@gmail.com>
2024-02-29 22:52:33 +0100

    successfully deferring the loading of data and rendering a loading component

--- src/pages/Films.js
+++ src/pages/Films.js
@@ -1,6 +1,8 @@
 import React from "react";
 import {
     useLoaderData,
+    defer,
+    Await,
 } from "react-router-dom";
 
 import "./Films.css";
@@ -8,23 +10,36 @@ import { getFilmData } from "../data";
 import FilmCard from "../components/FilmCard";
 
 export function loader() {
-    return getFilmData()
+    return defer({ films: getFilmData() })
 }
 
 export default function Films() {
 
-    const filmData = useLoaderData();
-    console.log(filmData);
+    const dataPromise = useLoaderData();
 
-    const filmElements = filmData.map(film => (
-        <FilmCard data={film} />
-    ))
+    
+    function renderFilmElements(films) {
+        
+        const filmElements = films.map(film => (
+            <FilmCard data={film} />
+        ))
 
-    return (
-        <>
+        return (
             <div className="films--container">
                 {filmElements}
             </div>
+        )
+    }
+
+    return (
+        <>
+            <h1>My films :)</h1>
+            <React.Suspense fallback={<h3>Loading films...</h3>}>
+                <Await resolve={dataPromise.films}>
+                    {renderFilmElements}
+                </Await>
+            </React.Suspense>
         </>
     )
+
 }
