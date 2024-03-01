693bf292e1f918ab5d31051e66908cd9cfb28748
trebordeveloping <robert3cormack@gmail.com>
2024-03-01 21:46:31 +0100

    basic routing and nav setup

--- src/index.js
+++ src/index.js
@@ -12,15 +12,23 @@ import Layout from "./components/Layout";
 import About from './pages/About';
 import Films, { loader as filmsLoader } from './pages/Films/Films';
 import FilmDetail from './pages/Films/FilmDetail';
-
+import Account from "./pages/Account/Account";
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<Layout />}>
+
     <Route index element={<h1>Home</h1>} />
     <Route path="about" element={<About />} />
-    <Route path="account" element={<h1>Account</h1>} />
+
     <Route path="films" element={<Films />} loader={filmsLoader} />
     <Route path="films/:title" element={<FilmDetail />} />
+
+    <Route path="account" element={<Account />}>
+      <Route index element={<h1>favourite films go here</h1>} />
+      <Route path="reviews" element={<h1>reviews go here</h1>} />
+      <Route path="profile" element={<h1>profile info goes here</h1>} />
+    </Route>
+
   </Route>
 ))
 
---
+++ src/pages/Account/Account.js
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+    Outlet,
+    NavLink,
+} from "react-router-dom";
+
+export default function Account() {
+
+    const activeStyle = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#AFAFAF",
+    }
+
+    return (
+        <>
+            <nav className="account--nav">
+                <NavLink
+                    to="."
+                    end
+                    style={({ isActive }) => isActive ? activeStyle : null}
+                >
+                    Favourites
+                </NavLink>
+
+                <NavLink
+                    to="reviews"
+                    style={({ isActive }) => isActive ? activeStyle : null}
+                >
+                    Reviews
+                </NavLink>
+
+                <NavLink
+                    to="profile"
+                    style={({ isActive }) => isActive ? activeStyle : null}
+                >
+                    Profile
+                </NavLink>
+            </nav>
+            <Outlet />
+        </>
+    )
+}
